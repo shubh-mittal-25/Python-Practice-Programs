955b6bd50e7dcf060b357abe72602179ab5434e6
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-05-23 16:09:16 +0530

    create snake body

--- Turtle Graphics/Snake_Game.py
+++ Turtle Graphics/Snake_Game.py
@@ -1,8 +1,15 @@
 import turtle
 
 screen = turtle.Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=700, height=700)
 screen.bgcolor("black")
 screen.title("Snake Game")
+
+starting_positions = [(0,0),(-20,0),(-40,0)]
+for position in starting_positions:
+    timmy = turtle.Turtle()
+    timmy.color("white")
+    timmy.shape("square")
+    timmy.teleport(position[0],position[1])
 
 screen.exitonclick()
