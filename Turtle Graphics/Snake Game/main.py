297da7b37b9d9e536a71cd71e1a350a7edea2d26
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-05-23 19:11:49 +0530

    detect collision with wall and tail

--- Turtle Graphics/Snake Game/main.py
+++ Turtle Graphics/Snake Game/main.py
@@ -25,10 +25,21 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-    #Detect Collision with food
+
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
