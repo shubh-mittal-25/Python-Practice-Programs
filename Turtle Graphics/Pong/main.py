2ff484b2514516074468105f6f883577c7ba259f
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-05-27 17:19:15 +0530

    Scorekeeping

--- Turtle Graphics/Pong/main.py
+++ Turtle Graphics/Pong/main.py
@@ -2,6 +2,7 @@ import turtle
 import time
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 screen = turtle.Screen()
 screen.setup(width=800, height=600)
@@ -12,16 +13,17 @@ screen.tracer(0)
 right_paddle = Paddle((380, 0))
 left_paddle = Paddle((-380, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(right_paddle.go_up, "Up")
-screen.onkey(right_paddle.go_down, "Down")
-screen.onkey(left_paddle.go_up, "w")
-screen.onkey(left_paddle.go_down, "s")
+screen.onkeypress(right_paddle.go_up, "Up")
+screen.onkeypress(right_paddle.go_down, "Down")
+screen.onkeypress(left_paddle.go_up, "w")
+screen.onkeypress(left_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -36,9 +38,11 @@ while game_is_on:
     #Detect when right paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.left_point()
 
     #Detect when left paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.right_point()
 
 screen.exitonclick()
