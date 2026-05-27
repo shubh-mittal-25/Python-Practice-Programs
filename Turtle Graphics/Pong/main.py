7d2740073df58836281a7e3a9b3bc4484bc0930a
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-05-27 16:53:42 +0530

    Detect when paddle misses

--- Turtle Graphics/Pong/main.py
+++ Turtle Graphics/Pong/main.py
@@ -33,4 +33,12 @@ while game_is_on:
     if ball.distance(right_paddle) < 50 and ball.xcor() > 350 or ball.distance(left_paddle) < 50 and ball.xcor() < -350:
         ball.bounce_x()
 
+    #Detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    #Detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 screen.exitonclick()
