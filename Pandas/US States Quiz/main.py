ec97de9015cb82967b2f90d9421d53c6054c9421
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-05-31 17:27:53 +0530

    India State game completed

--- Pandas/US States Quiz/main.py
+++ Pandas/US States Quiz/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("US States Quiz")
@@ -6,12 +7,32 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
 # # Code to get mouse click coordinates on the image
 # def get_mouse_click_coor(x,y):
 #     print(x,y)
 # turtle.onscreenclick(get_mouse_click_coor)
 
-answer_state = screen.textinput(title="Guess the state", prompt="Enter a state's name : ")
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="Enter a state's name : ")
+    answer_state = answer_state.title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        pointer = turtle.Turtle()
+        pointer.hideturtle()
+        pointer.penup()
+        state_data = data[data.state == answer_state]
+        pointer.goto(state_data.x.item(), state_data.y.item())
+        pointer.write(answer_state)
 
-
-turtle.mainloop()
