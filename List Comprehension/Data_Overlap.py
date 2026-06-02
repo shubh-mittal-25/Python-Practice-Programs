33673959940d9c9839e8f1dd4610fddee14b3823
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-02 23:13:09 +0530

    list comp(3) - result

--- List Comprehension/Data_Overlap.py
+++ List Comprehension/Data_Overlap.py
@@ -1,6 +1,11 @@
 with open("file1.txt") as f1:
     list1 = f1.readlines()
 
-result =
+with open("file2.txt") as f2:
+    list2 = f2.readlines()
 
+list1 = [int(i) for i in list1]
+list2 = [int(i) for i in list2]
+
+result = [num for num in list1 if num in list2]
 print(result)
