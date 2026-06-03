b7ace5be0217e9370acbba8b508e8d057396080d
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-03 15:53:01 +0530

    used list comprehension

--- Pandas/US States Quiz/main.py
+++ Pandas/US States Quiz/main.py
@@ -20,10 +20,7 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="Enter a state's name : ")
     answer_state = answer_state.title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
