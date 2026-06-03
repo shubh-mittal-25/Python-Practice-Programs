0437dfcde484188b3b7769697694b230e9551340
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-03 16:36:14 +0530

    updated

--- Pandas/NATO Alphabet/main.py
+++ Pandas/NATO Alphabet/main.py
@@ -1,6 +1,5 @@
 import pandas
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
-nato_data_frame = pandas.DataFrame(data)
+nato_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter:row.code for (index,row) in nato_data_frame.iterrows()}
 
