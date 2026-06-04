765d6f3f95ed464c25525d1684cdae4103533849
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-04 17:58:38 +0530

    pomodoro (countdown mechanism)

--- Tkinter/Pomodoro/main.py
+++ Tkinter/Pomodoro/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -13,8 +13,17 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def start_timer():
+    countdown(5 * 60)
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def countdown(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000,countdown, count-1)
 
 # ---------------------------- UI SETUP -----------------------------------#
 window = Tk()
@@ -24,14 +33,14 @@ window.config(padx=100, pady=50 , bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW , highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME , 30 , "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME , 30 , "bold"))
 canvas.grid(row=1, column=1)
 
 timer_label = Label(text="Timer")
 timer_label.config(bg=YELLOW, fg=GREEN, font=(FONT_NAME,50,"bold"))
 timer_label.grid(row=0, column=1)
 
-start_button = Button(text="Start")
+start_button = Button(text="Start" , command=start_timer)
 start_button.config(font=(FONT_NAME,10) , highlightthickness=0)
 start_button.grid(row=2, column=0)
 
