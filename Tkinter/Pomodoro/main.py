7f780d4eed37affd597f145c6034eca19a863831
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-04 17:32:51 +0530

    pomodoro (widgets)

--- Tkinter/Pomodoro/main.py
+++ Tkinter/Pomodoro/main.py
@@ -1,7 +1,5 @@
 from tkinter import *
 
-from Tkinter.example import window
-
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -18,4 +16,31 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
-# ---------------------------- UI SETUP ------------------------------- #
+# ---------------------------- UI SETUP -----------------------------------#
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50 , bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW , highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME , 30 , "bold"))
+canvas.grid(row=1, column=1)
+
+timer_label = Label(text="Timer")
+timer_label.config(bg=YELLOW, fg=GREEN, font=(FONT_NAME,50,"bold"))
+timer_label.grid(row=0, column=1)
+
+start_button = Button(text="Start")
+start_button.config(font=(FONT_NAME,10) , highlightthickness=0)
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset")
+reset_button.config(font=(FONT_NAME,10) , highlightthickness=0)
+reset_button.grid(row=2, column=2)
+
+checkmark_label = Label(text="✔")
+checkmark_label.config(bg=YELLOW, fg=GREEN, font=(FONT_NAME,15))
+checkmark_label.grid(row=3, column=1)
+
+window.mainloop()
