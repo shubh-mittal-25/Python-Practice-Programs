3764780844cea1d8137f7b5efc31a99ce541c805
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-05 16:22:37 +0530

    Ass popups and generate random password

--- Tkinter/Password_Manager/main.py
+++ Tkinter/Password_Manager/main.py
@@ -1,15 +1,41 @@
 from tkinter import *
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+from tkinter import messagebox
+import random
+import pyperclip
 
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [random.choice(letters) for _ in range(random.randint(8,10))]
+    password_numbers = [random.choice(numbers) for _ in range(random.randint(2,4))]
+    password_symbols = [random.choice(symbols) for _ in range(random.randint(2,4))]
+    password_list = password_letters + password_numbers + password_symbols
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    password_textField.delete(0, END)
+    password_textField.insert(0,password)
+    pyperclip.copy(password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website_name = website_textField.get()
     email = email_textField.get()
-    password = password_textField.get()
-    with open("data.txt", "a") as file:
-        file.write(f"{website_name} | {email} | {password} \n")
-    website_textField.delete(0, "end")
-    password_textField.delete(0, "end")
+    password_text = password_textField.get()
+    if website_name == "" or email == "" or password_text == "":
+        messagebox.showerror("Error", "Please enter all fields")
+    else:
+        is_ok = messagebox.askokcancel(title=website_name, message=f"These are the details entered:\n"
+                                                           f"Email: {email}\n"
+                                                           f"Password: {password_text}\n"
+                                                           f"Is it okay to save?")
+        if is_ok:
+            with open("data.txt", "a") as file:
+                file.write(f"{website_name} | {email} | {password_text} \n")
+            website_textField.delete(0, "end")
+            password_textField.delete(0, "end")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -40,7 +66,7 @@ password_textField = Entry(width=32)
 password_textField.grid(column=1, row=3)
 
 #BUTTONS
-gen_pass_button = Button(text="Generate Password")
+gen_pass_button = Button(text="Generate Password" , command=generate_password)
 gen_pass_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width = 43, command = save)
