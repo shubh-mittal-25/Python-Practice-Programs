4965e941cbb59a15f9cabb55f8fb6a72095397df
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-06 18:44:08 +0530

    Create new flashcards

--- Tkinter/Hindi-English Flash Cards/main.py
+++ Tkinter/Hindi-English Flash Cards/main.py
@@ -1,8 +1,18 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+data = pandas.read_csv("data/Hindi_to_English.csv")
+to_learn = data.to_dict(orient="records")
 
+# <----------------------------NEW FLASH CARDS------------------------------->
+def flash_card():
+    current_card = random.choice(to_learn)
+    word = current_card["Hindi"]
+    canvas.itemconfig(Hindi_text, text="Hindi")
+    canvas.itemconfig(Hindi_word, text=word)
 
 # <----------------------------UI DESIGN------------------------------------->
 window = Tk()
@@ -13,16 +23,20 @@ window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 canvas = Canvas(width=800, height=530, bg=BACKGROUND_COLOR, highlightthickness=0)
 flashcard_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 265, image=flashcard_img)
-Hindi_text = canvas.create_text(400, 150, text="Hindi", fill="black", font=("Ariel",40,"italic"))
-Hindi_word = canvas.create_text(400, 293, text="है", fill="black", font=("Ariel",60,"bold"))
+Hindi_text = canvas.create_text(400, 150, text="", fill="black", font=("Ariel",40,"italic"))
+Hindi_word = canvas.create_text(400, 293, text="", fill="black", font=("Ariel",60,"bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 #BUTTONS
 right_img = PhotoImage(file="images/right.png")
 wrong_img = PhotoImage(file="images/wrong.png")
-right_button = Button(image=right_img, bg=BACKGROUND_COLOR, highlightthickness=0)
+right_button = Button(image=right_img, bg=BACKGROUND_COLOR, highlightthickness=0, command=flash_card)
 right_button.grid(row=1, column=0)
-wrong_button = Button(image=wrong_img, bg=BACKGROUND_COLOR,highlightthickness=0)
+wrong_button = Button(image=wrong_img, bg=BACKGROUND_COLOR,highlightthickness=0, command=flash_card)
+wrong_button.grid(row=1, column=1)
 wrong_button.grid(row=1, column=1)
 
+flash_card()
+
 window.mainloop()
+
