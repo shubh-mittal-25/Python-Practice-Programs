d06a3874dabdcdfeb525fe0af3bde6462f3222b9
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-06 12:10:17 +0530

    exception handling

--- Pandas/NATO Alphabet/main.py
+++ Pandas/NATO Alphabet/main.py
@@ -1,10 +1,19 @@
 import pandas
+
 nato_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter:row.code for (index,row) in nato_data_frame.iterrows()}
 
-word = input("Enter a word: ")
-code_word = [nato_dict[letter.upper()] for letter in word]
-print(code_word)
+def generate_phonetic():
+    word = input("Enter a word: ")
+    try:
+        code_word = [nato_dict[letter.upper()] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(code_word)
+
+generate_phonetic()
 
 
