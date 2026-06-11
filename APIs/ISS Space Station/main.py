4ea6a2fbe9b6158b9eb110f39939b2fb38c100a9
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-11 21:55:54 +0530

    sunrise,sunset

--- APIs/ISS Space Station/main.py
+++ APIs/ISS Space Station/main.py
@@ -1,9 +1,23 @@
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 28.350215
+MY_LONG = 79.423187
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# latitude = response.json()["iss_position"]["latitude"]
+# longitude = response.json()["iss_position"]["longitude"]
+#
+# iss_position = (latitude, longitude)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json" , params=parameters)
 response.raise_for_status()
-
-latitude = response.json()["iss_position"]["latitude"]
-longitude = response.json()["iss_position"]["longitude"]
-
-iss_position = (latitude, longitude)
+data = response.json()
+sunrise_time = data["results"]["sunrise"]
+sunset_time = data["results"]["sunset"]
