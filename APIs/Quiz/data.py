2efcab456da0d047236188f3b31f46e65b821900
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-12 15:15:49 +0530

    completed

--- APIs/Quiz/data.py
+++ APIs/Quiz/data.py
@@ -1,7 +1,8 @@
 import requests
-
 parameters = {
     "amount":10,
+    "category":18,
+    "difficulty":"easy",
     "type":"boolean",
 }
 response = requests.get(url="https://opentdb.com/api.php" , params=parameters)
