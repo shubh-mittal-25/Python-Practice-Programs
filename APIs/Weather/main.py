86697b64a4ce7324e6403f09c85737d278a67e5d
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-13 17:06:47 +0530

    weather API done

--- APIs/Weather/main.py
+++ APIs/Weather/main.py
@@ -11,9 +11,19 @@ parameters = {
     "lat": LATITUDE,
     "lon": LONGITUDE,
     "appid": app_key,
+    "cnt": 8,
 }
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
 response.raise_for_status()
-data = response.json()
-print(data)
+weather_data = response.json()
+
+print("WEATHER")
+for i in range(8):
+    print(f"Date/Time: {weather_data['list'][i]['dt_txt']} | "
+          f"Temperature: {round(weather_data['list'][i]['main']['temp'] - 273.15 , 2)} | "
+          f"Feels Like: {round(weather_data['list'][i]['main']['feels_like'] - 273.15 , 2)} | "
+          f"Humidity: {weather_data['list'][i]['main']['humidity']}% | "
+          f"Wind Speed: {weather_data['list'][i]['wind']['speed']} m/s | "
+          f"{weather_data["list"][i]["weather"][0]["main"]} | "
+          f"{weather_data["list"][i]["weather"][0]["description"]}")
