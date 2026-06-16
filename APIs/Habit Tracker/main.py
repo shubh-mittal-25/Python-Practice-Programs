4c05f5f4789cb810df7944a6ad7abb7a30ebd881
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-16 22:14:22 +0530

    create new graph using pixela

--- APIs/Habit Tracker/main.py
+++ APIs/Habit Tracker/main.py
@@ -4,8 +4,8 @@ import dotenv
 
 dotenv.load_dotenv()
 api_key = os.getenv("API_KEY")
-user_name = os.getenv("USERNAME")
-
+username = os.getenv("PIXELA_USERNAME")
+#-----------------------CREATE ACCOUNT---------------------------------------
 pixela_endpoint = "https://pixe.la/v1/users"
 # user_parameters = {
 #     "token": api_key,
@@ -16,4 +16,20 @@ pixela_endpoint = "https://pixe.la/v1/users"
 #
 # response = requests.post(url=pixela_endpoint, json=user_parameters)
 # print(response.text)
+
+#-----------------------CREATE GRAPH---------------------------------------
+# graph_endpoint = f"{pixela_endpoint}/{username}/graphs"
+# print(username)
+# headers = {"X-USER-TOKEN": str(api_key)}
+# graph_parameters = {
+#     "id": "graph1",
+#     "name": "Cycling Graph",
+#     "unit": "Km",
+#     "type": "float",
+#     "color": "momiji"
+# }
+#
+# response = requests.post(url=graph_endpoint, json=graph_parameters, headers=headers)
+# print(response.text)
+
 
