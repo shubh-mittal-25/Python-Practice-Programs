1b7d815c103cefbc00e01b9be1f13d010a057bee
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-19 18:29:49 +0530

    update main

--- APIs/Flight Tracker/main.py
+++ APIs/Flight Tracker/main.py
@@ -3,12 +3,18 @@ from data_manager import DataManager
 from flight_search import FlightSearch
 import requests_cache
 from datetime import datetime, timedelta
+from flight_data import find_cheapest_flight
 
-requests_cache.install_cache("flight_cache", expire_after=3600)
-
-# data_manager = DataManager()
-# sheet_data = data_manager.get_destination_data()
-# pprint(sheet_data)
+requests_cache.install_cache(
+    "flight_cache",
+    urls_expire_after={
+        "*.sheety.co*": requests_cache.DO_NOT_CACHE,
+        "*": 3600,
+    }
+)
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+pprint(sheet_data)
 
 tomorrow = datetime.now() + timedelta(days=1)
 six_months_from_today = datetime.now() + timedelta(days=(6*30))
@@ -21,3 +27,10 @@ flights = flight_search.check_flights(
     to_time=six_months_from_today,
 )
 pprint(flights)
+
+cheapest_flight = find_cheapest_flight(flights, return_date=six_months_from_today.strftime("%Y-%m-%d"))
+pprint(f"{sheet_data[0]['city']}: GBP {cheapest_flight.price}")
+
+if cheapest_flight.price != "N/A" and cheapest_flight.price < sheet_data[0]["lowestPrice"]:
+    pprint(f"Lower price flight found to {sheet_data[0]['city']}!")
+    data_manager.update_lowest_price(sheet_data[0]["id"], cheapest_flight.price)
