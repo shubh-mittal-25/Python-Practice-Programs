b5bfdd875b985b81a88ab532c180a1c7c870f42e
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-19 10:23:11 +0530

    add requests_cache

--- APIs/Flight Tracker/main.py
+++ APIs/Flight Tracker/main.py
@@ -1,5 +1,8 @@
 from pprint import pprint
 from data_manager import DataManager
+import requests_cache
+
+requests_cache.install_cache("flight_cache", expire_after=3600)
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
