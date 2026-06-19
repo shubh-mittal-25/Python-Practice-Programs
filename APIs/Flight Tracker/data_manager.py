09e8c1a97a2de127261688cb06d8aeb96caf50d8
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-19 10:08:42 +0530

    get data from google sheets using sheety API

--- APIs/Flight Tracker/data_manager.py
+++ APIs/Flight Tracker/data_manager.py
@@ -1,3 +1,23 @@
+import os
+import dotenv
+from requests.auth import HTTPBasicAuth
+import requests
+
+dotenv.load_dotenv()
+
+SHEETY_ENDPOINT = "https://api.sheety.co/b0f6d9080e2e5098265356b410516807/flightDeals/prices"
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self):
+        self.user = os.getenv("SHEETY_USERNAME")
+        self.password = os.getenv("SHEETY_PASSWORD")
+        self.authorization = HTTPBasicAuth(self.user, self.password)
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_ENDPOINT, auth=self.authorization)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
--- APIs/Flight Tracker/main.py
+++ APIs/Flight Tracker/main.py
@@ -1,1 +1,6 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from pprint import pprint
+from data_manager import DataManager
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+pprint(sheet_data)
