5d775ad11d849040231d79ed65b91ca861fc21bf
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-19 18:02:48 +0530

    get flight data

--- APIs/Flight Tracker/flight_search.py
+++ APIs/Flight Tracker/flight_search.py
@@ -1,3 +1,35 @@
+import os
+import dotenv
+import requests
+
+dotenv.load_dotenv()
+SERP_API_ENDPOINT = "https://serpapi.com/search"
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+
+    def __init__(self):
+        self.api_key = os.getenv("SERP_API_KEY")
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        query = {
+          "engine": "google_flights",
+          "departure_id": origin_city_code,
+          "arrival_id": destination_city_code,
+          "outbound_date": from_time.strftime("%Y-%m-%d"),
+          "return_date": to_time.strftime("%Y-%m-%d"),
+          "type": "1",
+          "adults": "1",
+          "currency": "GBP",
+          "api_key": self.api_key,
+         }
+        response = requests.get(SERP_API_ENDPOINT, params=query)
+        if response.status_code != 200:
+            print(f"check_flights() response code: {response.status_code}")
+            return None
+
+        data = response.json()
+        if "error" in data:
+            print(f"API error: {data['error']}")
+            return None
+        return data
+
--- APIs/Flight Tracker/main.py
+++ APIs/Flight Tracker/main.py
@@ -1,9 +1,23 @@
 from pprint import pprint
 from data_manager import DataManager
+from flight_search import FlightSearch
 import requests_cache
+from datetime import datetime, timedelta
 
 requests_cache.install_cache("flight_cache", expire_after=3600)
 
-data_manager = DataManager()
-sheet_data = data_manager.get_destination_data()
-pprint(sheet_data)
+# data_manager = DataManager()
+# sheet_data = data_manager.get_destination_data()
+# pprint(sheet_data)
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_months_from_today = datetime.now() + timedelta(days=(6*30))
+
+flight_search = FlightSearch()
+flights = flight_search.check_flights(
+    origin_city_code="LHR",
+    destination_city_code="CDG",
+    from_time=tomorrow,
+    to_time=six_months_from_today,
+)
+pprint(flights)
