3a718b1234e4501b83e63af133831076e97a6fc7
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-25 17:49:31 +0530

    extracting elements

--- Beautiful Soup/main.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open("website.html") as file:
-    contents = file.read()
-
-soup = BeautifulSoup(contents, "html.parser")
-print(soup.title)
-print(soup.title.name)
-print(soup.title.string)
---
+++ Beautiful Soup/scraping html code/main.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+
+with open("website.html") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+print(soup.title)
+print(soup.title.name)
+print(soup.title.string)
+
+all_anchor_tags = soup.find_all(name='a')
+
+for tag in all_anchor_tags:
+    print(tag.get_text())
+    print(tag.get('href'))
+
+heading = soup.find(name='h1', id="name")
+print(heading)
+
+heading_2 = soup.find(name='h3', class_="heading")
+print(heading_2)
+
+company_url = soup.select_one(selector="p a")
+print(company_url)
