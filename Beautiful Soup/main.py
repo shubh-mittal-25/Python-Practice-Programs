f74584bbc1d9c0058cdd623f3b666a702da7ef0c
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-25 15:29:21 +0530

    making soup

---
+++ Beautiful Soup/main.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+
+with open("website.html") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+print(soup.title)
+print(soup.title.name)
+print(soup.title.string)
