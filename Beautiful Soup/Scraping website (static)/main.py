5a99a6372310e2902e209b166f56b090517e9206
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-06-27 23:39:39 +0530

    working

--- Beautiful Soup/Scraping website (static)/main.py
+++ Beautiful Soup/Scraping website (static)/main.py
@@ -5,5 +5,17 @@ response = requests.get("https://appbrewery.github.io/news.ycombinator.com/")
 webpage = response.text
 
 soup = BeautifulSoup(webpage, "html.parser")
-article_title = soup.find("a", class_="storylink").getText()
-print(article_title)
+articles = soup.find_all("a", class_="storylink")
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
+
+article_upvotes = [score.getText() for score in soup.find_all("span", class_="score")]
+
+print(article_texts)
+print(article_links)
+print(article_upvotes)
