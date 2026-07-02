d792d79e0d0c4905a3c0e5b544a65000a31149aa
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-07-03 00:30:34 +0530

    done

--- Musical Time Machine Project/top_100.py
+++ Musical Time Machine Project/top_100.py
@@ -12,32 +12,35 @@ print(top_100_list)
 
 yt = YTMusic("browser.json")
 
-PLAYLIST_NAME = f"{date} Billboard 100"
+playlists = yt.get_library_playlists()
+print(f"Found {len(playlists)} playlists in your library.")
 
-# Check if playlist already exists
-playlist_id = None
-playlists = yt.get_library_playlists(limit=100)
-
-for p in playlists:
-    if p["title"] == PLAYLIST_NAME:
-        playlist_id = p["playlistId"]
-        break
-
-if playlist_id:
-    print("This playlist already exists.")
-else:
-    playlist_id = yt.create_playlist(
-        PLAYLIST_NAME,
-        f"Playlist with the hottest songs from {date}",
-        privacy_status="PRIVATE",
-    )
-    print("Playlist created.")
-
-    # Search and add each song
-    for song in top_100_list:
-        try:
-            search_results = yt.search(song, filter="songs", limit=1)
-            yt.add_playlist_items(playlist_id, [search_results[0]["videoId"]])
-            print(f"Added: {song}")
-        except Exception as e:
-            print(f"Skipped: {song} | Reason: {e}")
+# PLAYLIST_NAME = f"{date} Billboard 100"
+#
+# # Check if playlist already exists
+# playlist_id = None
+# playlists = yt.get_library_playlists(limit=100)
+#
+# for p in playlists:
+#     if p["title"] == PLAYLIST_NAME:
+#         playlist_id = p["playlistId"]
+#         break
+#
+# if playlist_id:
+#     print("This playlist already exists.")
+# else:
+#     playlist_id = yt.create_playlist(
+#         PLAYLIST_NAME,
+#         f"Playlist with the hottest songs from {date}",
+#         privacy_status="PRIVATE",
+#     )
+#     print("Playlist created.")
+#
+#     # Search and add each song
+#     for song in top_100_list:
+#         try:
+#             search_results = yt.search(song, filter="songs", limit=1)
+#             yt.add_playlist_items(playlist_id, [search_results[0]["videoId"]])
+#             print(f"Added: {song}")
+#         except Exception as e:
+#             print(f"Skipped: {song} | Reason: {e}")
