3c6c6bedd737d9698a46676679118962420ce054
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-07-02 23:55:33 +0530

    using ytmusicapi

--- Musical Time Machine Project/main.py
+++ Musical Time Machine Project/main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+from ytmusicapi import YTMusic
 
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD :\n")
 response = requests.get(f"https://appbrewery.github.io/bakeboard-hot-100/{date}/")
@@ -7,6 +8,8 @@ webpage = response.text
 soup = BeautifulSoup(webpage, "html.parser")
 
 top_100_list = [song.getText() for song in soup.find_all('h3' , class_='chart-entry__title')]
-
-
 print(top_100_list)
+
+yt = YTMusic("browser.json")
+playlists = yt.get_library_playlists()
+print(f"Found {len(playlists)} playlists in your library.")
