8bb183f369616add69fdc11719e513213f291ee6
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-07-02 23:59:39 +0530

    create a playlist

--- Musical Time Machine Project/main.py
+++ Musical Time Machine Project/main.py
@@ -13,3 +13,24 @@ print(top_100_list)
 yt = YTMusic("browser.json")
 playlists = yt.get_library_playlists()
 print(f"Found {len(playlists)} playlists in your library.")
+
+PLAYLIST_NAME = f"{date} Billboard 100"
+
+# Check if playlist already exists
+playlist_id = None
+playlists = yt.get_library_playlists(limit=100)
+
+for p in playlists:
+    if p["title"] == PLAYLIST_NAME:
+        playlist_id = p["playlistId"]
+        break
+
+if playlist_id:
+    print("This playlist already exists.")
+else:
+    playlist_id = yt.create_playlist(
+        PLAYLIST_NAME,
+        f"Playlist with the hottest songs from {date}",
+        privacy_status="PRIVATE",
+    )
+    print("Playlist created.")
