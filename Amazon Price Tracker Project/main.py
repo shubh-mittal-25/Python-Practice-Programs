b896bff8521ad21e60b600af517f08a8a971aea7
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-07-03 22:21:35 +0530

    send email

--- Amazon Price Tracker Project/main.py
+++ Amazon Price Tracker Project/main.py
@@ -1,9 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
+import os
+import dotenv
+import smtplib
+
+BUY_PRICE = 100
+dotenv.load_dotenv()
+sender = os.getenv("SENDER_EMAIL")
+password = os.getenv("PASSWORD")
 
 response = requests.get("https://appbrewery.github.io/instant_pot/")
 webpage = response.text
 
 soup = BeautifulSoup(webpage, "html.parser")
-price = f"{soup.find("span", class_="a-price-whole").getText()}{soup.find("span", class_="a-price-fraction").getText()}"
-print(price)
+price = float(f"{soup.find("span", class_="a-price-whole").getText()}{soup.find("span", class_="a-price-fraction").getText()}")
+title = soup.find(id="productTitle").get_text().strip()
+
+
+if price < BUY_PRICE:
+    message = f"{title} is on sale for {price}!"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=sender, password=password)
+        connection.sendmail(
+            from_addr=sender,
+            to_addrs=sender,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\nURL".encode("utf-8")
+        )
