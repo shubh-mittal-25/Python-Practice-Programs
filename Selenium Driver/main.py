93ce6f85bcb01c44aa188d2281dccc5285ee098b
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-07-04 23:06:05 +0530

    scrape website data

--- Selenium Driver/main.py
+++ Selenium Driver/main.py
@@ -1,3 +1,5 @@
+from operator import index
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -6,10 +8,19 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach" , True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.amazon.in/Native-M2-Pro-Dispensing-Mineraliser/dp/B0G4CHKBGP/?_encoding=UTF8&ref_=pd_hp_d_btf_homenKitchen")
+driver.get("https://www.python.org/")
 
-price = driver.find_element(By.CLASS_NAME, value="a-price-whole").text
-print(price)
+event_times = driver.find_elements(By.CSS_SELECTOR, value=".event-widget time")
+event_names = driver.find_elements(By.CSS_SELECTOR, value=".event-widget li a")
+events = {}
+
+for n in range(len(event_times)):
+    events[n] = {
+        "time": event_times[n].text,
+        "name": event_names[n].text
+    }
+print(events)
+
 
 # driver.close()
 driver.quit()
