c45527c8eac152feb05dc8281020a5bd4ccffc8b
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-07-04 23:25:32 +0530

    scrape wiki

--- Selenium Driver/interaction.py
+++ Selenium Driver/interaction.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach" , True)
@@ -7,7 +8,9 @@ chrome_options.add_experimental_option("detach" , True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-articles = driver.find_element(By.ID, "mwDw").text
-print(articles)
+search_button = driver.find_element(By.LINK_TEXT, "Search")
+search_button.click()
 
-driver.quit()
+#USING SEARCH BAR
+search = driver.find_element(By.NAME, "search")
+search.send_keys("Python" , Keys.ENTER)
