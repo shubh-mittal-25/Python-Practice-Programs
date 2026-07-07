2049bf4afe016628ecf8f356904eb3776fa7caab
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-07-07 17:23:47 +0530

    login

--- Automation/Gym Booking/main.py
+++ Automation/Gym Booking/main.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 import os
 import dotenv
 
-dotenv.load_dotenv()
+loaded = dotenv.load_dotenv()
 gym_url = str(os.getenv("URL"))
-username = str(os.getenv("USERNAME"))
+username = str(os.getenv("GYM_USER"))
 password = str(os.getenv("PASSWORD"))
 
 chrome_options = webdriver.ChromeOptions()
@@ -15,3 +18,20 @@ chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(gym_url)
+
+wait = WebDriverWait(driver, 2)
+
+login_button=wait.until(ec.element_to_be_clickable((By.ID, "login-button")))
+login_button.click()
+
+email_input = wait.until(ec.presence_of_element_located((By.ID, "email-input")))
+email_input.clear()
+email_input.send_keys(username)
+password_input = driver.find_element(By.ID, "password-input")
+password_input.clear()
+password_input.send_keys(password)
+
+submit_btn = driver.find_element(By.ID, "submit-button")
+submit_btn.click()
+
+wait.until(ec.presence_of_element_located((By.ID, "schedule-page")))
