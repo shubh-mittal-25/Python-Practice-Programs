75454f43809913744b2365824bfc4c8ed1a9e5a3
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-07-09 19:44:02 +0530

    create class

--- Automation/Speed & X/main.py
+++ Automation/Speed & X/main.py
@@ -1,8 +1,25 @@
 import os
 from dotenv import load_dotenv
+from selenium import webdriver
 
 load_dotenv()
-email = str(os.getenv("Y_EMAIL"))
-password = str(os.getenv("Y_PASSWORD"))
+EMAIL = str(os.getenv("Y_EMAIL"))
+PASSWORD = str(os.getenv("Y_PASSWORD"))
 UPLOAD_SPEED = 100
 DOWNLOAD_SPEED = 100
+
+class InternetSpeedTwitterBot:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.upload_speed = 0
+        self.download_speed = 0
+
+    def get_internet_speed(self):
+        pass
+
+    def tweet_at_provider(self):
+        pass
+
+bot = InternetSpeedTwitterBot()
+bot.get_internet_speed()
+bot.tweet_at_provider()
