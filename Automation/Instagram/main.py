243ed9a9ba64ccf370f89642540062f4ff0ed1a5
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-07-10 17:53:58 +0530

    create class

--- Automation/Instagram/main.py
+++ Automation/Instagram/main.py
@@ -1,8 +1,31 @@
 import os
 from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 load_dotenv()
 username = str(os.getenv("SAN_EMAIL"))
 password = str(os.getenv("SAN_PASSWORD"))
 SIMILAR_ACCOUNT = ("chefsteps")
 URL = "https://app.100daysofpython.dev/services/share-a-naan"
+LOGIN_URL = f"{URL}/login"
+
+class InstaFollower:
+    def __init__(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+    def login(self):
+        pass
+
+    def find_followers(self):
+        pass
+
+    def follow(self):
+        pass
+
+bot = InstaFollower()
+bot.login()
+bot.find_followers()
+bot.follow()
