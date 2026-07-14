9505f61667aa08581a0d7047b78749e87ea300e9
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-07-14 23:20:59 +0530

    decorator for html tags

--- Flask/hello.py
+++ Flask/hello.py
@@ -3,6 +3,21 @@ from flask import Flask
 app = Flask(__name__)
 print(__name__)
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
+
 @app.route("/")
 def hello_world():
     return ('<h1 style="text-align:center">Hello, World!</h1>'
@@ -11,6 +26,9 @@ def hello_world():
 
 
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
 def say_bye():
     return "Bye, World!"
 
