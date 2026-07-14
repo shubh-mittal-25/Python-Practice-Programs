73cc99c99e20f88ec0c2b8533667a8c1b9ffd568
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-07-14 23:18:09 +0530

    Rendering html elements

--- Flask/hello.py
+++ Flask/hello.py
@@ -5,15 +5,20 @@ print(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return ('<h1 style="text-align:center">Hello, World!</h1>'
+            '<p>Hello, World!</p>'
+            '<img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbWhldWlyNzRiNmplYWoyY2F5anlqcTY2ZzNzM3ZqMWduODl0NnV3aSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/W1T9kHIsG9nbTU1P6y/giphy.gif">')
+
 
 @app.route("/bye")
 def say_bye():
     return "Bye, World!"
 
-@app.route("/username/<name>")
-def greet(name):
-    return f"Hello, {name}!"
+@app.route("/username/<name>/<int:age>")
+def greet(name,age):
+    return f"Hello, {name}! Your age is {age} years old!"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
