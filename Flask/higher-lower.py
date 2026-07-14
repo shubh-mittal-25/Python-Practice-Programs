b6075ffc32f40e8ab2aab3f0aeb8ddfb933195bd
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-07-14 23:46:40 +0530

    guess

--- Flask/higher-lower.py
+++ Flask/higher-lower.py
@@ -13,13 +13,13 @@ num = randint(0,9)
 @app.route("/<int:guess>")
 def check(guess):
     if guess < num:
-        return ('<h1>f"Your guess is low!"<h1>'
+        return ('<h1>Your guess is low!<h1>'
                 '<img src="https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif">')
     elif guess > num:
-        return ('<h1>f"Your guess is high!"<h1>'
+        return ('<h1>Your guess is high!<h1>'
                 '<img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif">')
     else:
-        return ('<h1>f"You are correct!"<h1>'
+        return ('<h1>You are correct!<h1>'
                 '<img src="https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif">')
 
 if __name__ == "__main__":
