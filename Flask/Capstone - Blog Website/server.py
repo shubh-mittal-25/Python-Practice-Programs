d61726d984a899a9a032f179172a2769546965f3
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-07-17 21:55:08 +0530

    jinja with api

--- Flask/Capstone - Blog Website/server.py
+++ Flask/Capstone - Blog Website/server.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template
-import random
-import datetime
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    random_number = random.randint(1,10)
-    current_year = datetime.datetime.now().year
-    return render_template("index.html", num=random_number, year=current_year)
+    return "<h1>HOMEPAGE</h1>"
+
+@app.route("/guess/<string:name>")
+def guess(name):
+    gender_response = requests.get("https://api.genderize.io", params={'name': name})
+    gender_response = gender_response.json()
+    gender = gender_response["gender"]
+
+    age_response = requests.get("https://api.agify.io", params={'name': name})
+    age_response = age_response.json()
+    age = age_response["age"]
+
+    return render_template("index.html",name=name, gender=gender, age=age)
 
 if __name__ == "__main__":
     app.run(debug=True)
