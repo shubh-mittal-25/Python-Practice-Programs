9fa86813138c996dab62328c1da4daefc4647512
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-07-17 21:29:23 +0530

    learning jinja

--- Flask/Capstone - Blog Website/server.py
+++ Flask/Capstone - Blog Website/server.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    random_number = random.randint(1,10)
+    return render_template("index.html", num=random_number)
 
 if __name__ == "__main__":
     app.run(debug=True)
