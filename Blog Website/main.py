6ffe4529e14030dd2a79236201e72b65616d36b9
shubh-mittal-25 <shubhmittal125@gmail.com>
2026-07-24 20:19:18 +0530

    fetch blog posts from api

--- Blog Website/main.py
+++ Blog Website/main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import requests
+
+posts = requests.get("https://api.npoint.io/ed9fa09616db62eae4a2").json()
 
 app = Flask(__name__)
 
 @app.route('/')
 def get_all_posts():
-    return render_template('index.html')
+    return render_template('index.html', all_posts = posts)
 
 @app.route('/about')
 def about():
